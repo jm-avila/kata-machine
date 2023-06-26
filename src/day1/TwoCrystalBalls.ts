5653bfbb92aa827a0f3dbdf0b994baef7f6b14b8
jose-avila-007 <jose.avila@rookiewise.com>
2023-06-26 22:28:13 +0200

    TwoCrystalBall

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,21 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let lenSqrt = Math.floor(Math.sqrt(breaks.length));
+    let low = 0;
+    let high = breaks.length;
 
+    do {
+        const index = low + lenSqrt;
+        if (breaks[index] === true) {
+            low = index;
+            continue;
+        }
+
+        for (let i = low; i < high; i++) {
+            if (breaks[i]) {
+                return i;
+            }
+        }
+        break;
+    } while (low < high);
+    return -1;
 }
