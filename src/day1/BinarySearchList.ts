707399ca39cdf2e3834a4df4534a044a4d3cd882
jose-avila-007 <jose.avila@rookiewise.com>
2023-06-26 21:49:15 +0200

    BinarySearchList

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let high = haystack.length;
+    let low = 0;
 
+    do {
+        const middle = Math.floor(low + (high - low) / 2);
+        const value = haystack[middle];
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            high = middle;
+        } else {
+            low = middle + 1;
+        }
+    } while (low < high);
+    return false;
 }
