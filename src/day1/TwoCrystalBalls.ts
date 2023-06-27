b75db05d088aa432997bfe86938cad1c873f86fa
jose-avila-007 <jose.avila@rookiewise.com>
2023-06-27 23:01:06 +0200

    TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,21 +1,41 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-    let lenSqrt = Math.floor(Math.sqrt(breaks.length));
-    let low = 0;
-    let high = breaks.length;
+    const jumpLength = Math.floor(Math.sqrt(breaks.length));
 
-    do {
-        const index = low + lenSqrt;
-        if (breaks[index] === true) {
-            low = index;
-            continue;
+    let i = jumpLength;
+    for (; i < breaks.length; i++) {
+        if (breaks[i]) {
+            return i;
         }
+    }
 
-        for (let i = low; i < high; i++) {
-            if (breaks[i]) {
-                return i;
-            }
+    i -= jumpLength;
+
+    for (; i < breaks.length; i++) {
+        if (breaks[i]) {
+            return i;
         }
-        break;
-    } while (low < high);
+    }
     return -1;
 }
+
+// export default function two_crystal_balls(breaks: boolean[]): number {
+//     let lenSqrt = Math.floor(Math.sqrt(breaks.length));
+//     let low = 0;
+//     let high = breaks.length;
+
+//     do {
+//         const index = low + lenSqrt;
+//         if (breaks[index] === true) {
+//             low = index;
+//             continue;
+//         }
+
+//         for (let i = low; i < high; i++) {
+//             if (breaks[i]) {
+//                 return i;
+//             }
+//         }
+//         break;
+//     } while (low < high);
+//     return -1;
+// }
