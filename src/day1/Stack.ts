83b459932d85039605add4ccc56cedcf8d204a6e
jose-avila-007 <jose.avila@rookiewise.com>
2023-06-28 20:41:07 +0200

    Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,28 @@
 export default class Stack<T> {
-    public length: number;
-
-    
-
-    constructor() {
-    }
+    public length: number = 0;
+    public head?: Node<T>;
 
     push(item: T): void {
-
-}
+        const node = new Node(item);
+        node.next = this.head;
+        this.head = node;
+        this.length++;
+    }
     pop(): T | undefined {
-
-}
+        if (!this.head) {
+            return;
+        }
+        const head = this.head;
+        this.head = this.head.next;
+        this.length--;
+        return head.value;
+    }
     peek(): T | undefined {
-
+        return this.head?.value;
+    }
 }
+
+class Node<T> {
+    public next?: Node<T>;
+    constructor(public value: T) {}
 }
