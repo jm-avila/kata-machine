c3a923c638eeba14c43cb64ef043c3f17f6affbd
jose-avila-007 <jose.avila@rookiewise.com>
2023-07-04 08:53:27 +0200

    BinaryTree Depth First Traversal

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,22 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+type BinaryNode<T> = {
+    value: T;
+    left: BinaryNode<T> | null;
+    right: BinaryNode<T> | null;
+};
 
+function traverse(node: BinaryNode<number> | null, path: number[]): number[] {
+    if (!node) return path;
+    // Pre
+
+    // Recurse
+    traverse(node.left, path);
+    path.push(node.value);
+    traverse(node.right, path);
+
+    // Post
+
+    return path;
+}
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    return traverse(head, []);
 }
--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,21 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+type BinaryNode<T> = {
+    value: T;
+    left: BinaryNode<T> | null;
+    right: BinaryNode<T> | null;
+};
 
+function traverse(node: BinaryNode<number> | null, path: number[]): number[] {
+    if (!node) return path;
+    // Pre
+
+    // Recurse
+    traverse(node.left, path);
+    traverse(node.right, path);
+
+    // Post
+    path.push(node.value);
+    return path;
+}
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return traverse(head, []);
 }
