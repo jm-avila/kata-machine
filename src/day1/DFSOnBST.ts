5755a9dfb4e3b7cd36184959d07ac6a3c8667ced
jose-avila-007 <jose.avila@rookiewise.com>
2023-07-05 13:12:15 +0200

    Depth First on BST

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,37 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(node: BinaryNode<number> | null, needle: number): boolean {
+    if (!node) return false;
 
+    if (node.value === needle) return true;
+
+    if (node.value > needle) {
+        return search(node.left, needle);
+    } else {
+        return search(node.right, needle);
+    }
 }
+
+export function dfsRecursive(
+    head: BinaryNode<number>,
+    needle: number,
+): boolean {
+    return search(head, needle);
+}
+
+export function dfsIterative(
+    head: BinaryNode<number>,
+    needle: number,
+): boolean {
+    const q: (BinaryNode<number> | null)[] = [head];
+    while (q.length) {
+        const node = q.shift();
+        if (!node) return false;
+
+        if (node.value === needle) return true;
+
+        if (node.left) q.push(node.left);
+        if (node.right) q.push(node.right);
+    }
+    return false;
+}
+
+export default dfsRecursive;
