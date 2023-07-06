85aba80157e04c34a9d9e88a4c95494f86694f6a
jose-avila-007 <jose.avila@rookiewise.com>
2023-07-06 18:01:29 +0200

    BFSGraphMatrix

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,48 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+export default function bfs(
+    graph: WeightedAdjacencyMatrix,
+    source: number,
+    needle: number,
+): number[] | null {
+    const seen = new Array(graph.length).fill(false);
+    const prev = new Array(graph.length).fill(-1);
 
+    seen[source] = true;
+    const q: number[] = [source];
+
+    do {
+        const current = q.shift() as number;
+        if (current === needle) {
+            break;
+        }
+
+        const adjacency = graph[current];
+        for (let i = 0; i < adjacency.length; i++) {
+            if (adjacency[i] === 0) {
+                continue;
+            }
+
+            if (seen[i]) {
+                continue;
+            }
+
+            seen[i] = true;
+            prev[i] = current;
+
+            q.push(i);
+        }
+        seen[current] = true;
+    } while (q.length);
+
+    let current = needle;
+    const out: number[] = [];
+
+    while (prev[current] !== -1) {
+        out.push(current);
+        current = prev[current];
+    }
+
+    if (out.length) {
+        return [source].concat(out.reverse());
+    }
+    return null;
 }
